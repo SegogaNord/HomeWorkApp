4930f2b2698a0da7227756254b9354aca56dc88c
Zinoviev <serg1866@mail.ru>
2022-04-25 19:41:27 +0300

    DZ - 1 Zinoviev

---
+++ src/main.java
@@ -0,0 +1,55 @@
+public class main {
+    public static void main(String[] args) {
+
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+        public static void printThreeWords() {
+            System.out.println("Orange");
+            System.out.println("Banana");
+            System.out.println("Apple");
+            }
+        public static void checkSumSign() {
+            int a = 10;
+            int b = -11;
+
+               if (a + b >= 0) {
+               System.out.println("Сумма положительная");
+            }
+            else {
+                System.out.println("Сумма отрицательная");
+            }
+        }
+        public static void printColor() {
+
+            int value;
+            value = 457;
+             if (value <= 0) {
+                System.out.println("Красный");
+             }
+             if (value > 0 && value <= 100) {
+                System.out.println("Желтый");
+             }
+             else {
+                System.out.println("Зеленый");
+             }
+
+
+        }
+        public static void compareNumbers() {
+        int a = 27;
+        int b = 27;
+          if (a >=b ) {
+               System.out.println("a >= b");
+          }
+          else {
+              System.out.println("a < b");
+          }
+
+
+        }
+
+    }
